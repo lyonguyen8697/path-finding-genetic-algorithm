219599697fcacc50657d6de31e7dfe76a8463266
Lyo Nguyen <mr.locasdfg@gmail.com>
2018-01-29 02:58:06 +0700

    Add round_vec2 and calc_point_on_line

--- utils.py
+++ utils.py
@@ -1,8 +1,12 @@
 import random
-
+from pygame.math import Vector2
 
 def random_vec2():
     return (random.random() * 2 - 1, random.random() * 2 - 1)
+
+
+def round_vec2(vector):
+    return (round(vector[0]), round(vector[1]))
 
 
 def choice_distribution(data, key, recalc=True):
@@ -18,4 +22,11 @@ def choice_distribution(data, key, recalc=True):
         if s >= r:
             return d
     return d
+
+
+def calc_point_on_line(start, end, distance):
+    start = Vector2(start)
+    end = Vector2(end)
+    u = (end.elementwise() - start.elementwise()).normalize()
+    return start.elementwise() + distance * u.elementwise()
 
